3992d230820a517214308e2769a1f6552e0c0056
Charlie Pelton <charliejp0311@gmail.com>
2020-09-15 11:46:12 -0500

    Done.

--- .history/src/index_20200915112750.js
+++ .history/src/index_20200915112750.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import CatComponent from './CatComponent';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent';
+import MouseComponent from './MouseComponent';
 
 ReactDOM.render(<App />, document.getElementById('root'));
